731482da6d2336b2dfb9a19335a331619db1a1cd
Aldonfirst <alchevck44@bk.ru>
2023-08-24 02:16:06 +0300

    12345

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const errorHandler = require('./middlewares/errorHandler');
 
 // const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
-const { PORT, DB_URL } = process.env;
+const { PORT = 3000, DB_URL } = process.env;
 mongoose.connect(DB_URL, { useNewUrlParser: true });
 
 const app = express();
