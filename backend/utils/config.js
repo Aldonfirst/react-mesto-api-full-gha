b8501a92e876eb8a8cb640aec8fc2575351b894a
Aldonfirst <alchevck44@bk.ru>
2023-08-25 03:05:45 +0300

    333

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,11 +2,10 @@ require('dotenv').config();
 
 const { JWT_SECRET } = process.env;
 const { PORT = '3000' } = process.env;
-const { DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
-const { NODE_ENV = 'production' } = process.env;
+const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
 module.exports = {
   JWT_SECRET,
   PORT,
-  DB_URL,
-  NODE_ENV,
+  DB_ADDRESS,
 };
